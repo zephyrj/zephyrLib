a57d9482b8baaa2f8993a7ed93b04c2931bd9261
zephyrj <jono.sykes15@gmail.com>
2018-01-06 16:23:43 +0000

    Don't bother with gnu-tm for now

--- cmakeModules/FindGNUTransactionalMemory.cmake
+++ cmakeModules/FindGNUTransactionalMemory.cmake
@@ -1,10 +1,12 @@
-# Find the gnu-tm development library.
+# Find the GNU Transactional Memory (libitm) library.
 #
 # GNUTransactionalMemory_LIBRARIES - Library for gnu-tm
-# GNUTransactionalMemory_FOUND	- True if gnu-tm found
+# GNUTransactionalMemory_FLAG - Compiler flag for gnu-tm
+# GNUTransactionalMemory_FOUND - True if gnu-tm found
+# TODO work out how to do this properly
 
 # Look for the library.
-FIND_LIBRARY(GNUTransactionalMemory_LIBRARY NAMES gnu-tm)
+FIND_LIBRARY(GNUTransactionalMemory_LIBRARY NAMES itm)
 
 # Handle the QUIETLY and REQUIRED arguments and set Systemd_FOUND to TRUE if all listed variables are TRUE.
 INCLUDE(FindPackageHandleStandardArgs)
@@ -13,8 +15,10 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(GNUTransactionalMemory DEFAULT_MSG GNUTransact
 # Copy the results to the output variables.
 IF(GNUTransactionalMemory_FOUND)
     SET(GNUTransactionalMemory_LIBRARIES ${GNUTransactionalMemory_LIBRARY})
+    SET(GNUTransactionalMemory_FLAG "-fgnu-tm")
 ELSE()
     SET(GNUTransactionalMemory_LIBRARIES)
+    SET(GNUTransactionalMemory_FLAG)
 ENDIF()
 
 MARK_AS_ADVANCED(GNUTransactionalMemory_LIBRARIES)
