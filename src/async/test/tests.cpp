c6c7747ffd9cb4f76a0e2a02842b8ea690267cd7
zephyrj <jono.sykes15@gmail.com>
2018-05-06 20:20:31 +0100

    Add basic ScopedCallbackHelper tests

--- src/async/test/tests.cpp
+++ src/async/test/tests.cpp
@@ -2,14 +2,107 @@
 
 #include <ScopedCallbackHelper.hpp>
 
-void func()
+void func(int& x)
 {
-  std::string x;
+  x++;
 }
 
-TEST_CASE("Compile please")
+SCENARIO("ScopedCallback tests with std::bind")
 {
-  zephyr::async::ScopedCallbackHelper x;
-  x.createCallback(std::bind(&func));
+  GIVEN("A callback helper")
+  {
+    auto callbackHelper = std::make_unique<zephyr::async::ScopedCallbackHelper>();
+
+    WHEN("We create a callback using std::bind and bound arguments with the callback helper")
+    {
+      int testInt = 0;
+      auto callback = callbackHelper->createCallback(std::bind(&func, std::ref(testInt)));
+
+      THEN("The callback should execute as expected when the helper is in scope")
+      {
+        callback();
+        CHECK(testInt == 1);
+
+        AND_THEN("The callback shouldn't execute when the helper goes out of scope")
+        {
+          callbackHelper.reset(nullptr);
+          callback();
+          CHECK(testInt == 1);
+        }
+      }
+    }
+
+    WHEN("We create a callback using std::bind and std::placeholder with the callback helper")
+    {
+      auto callback = callbackHelper->createCallback(std::bind(&func, std::placeholders::_1));
+
+      THEN("The callback should execute as expected")
+      {
+        int testInt = 0;
+        callback(testInt);
+        CHECK(testInt == 1);
+
+        AND_THEN("The callback shouldn't execute when the helper goes out of scope")
+        {
+          callbackHelper.reset(nullptr);
+          callback(testInt);
+          CHECK(testInt == 1);
+        }
+      }
+    }
+  }
+}
+
+SCENARIO("ScopedCallback tests with lambdas")
+{
+  GIVEN("A callback helper")
+  {
+    auto callbackHelper = std::make_unique<zephyr::async::ScopedCallbackHelper>();
+
+    WHEN("We create a callback using a simple lambda with captured arguments with the callback helper")
+    {
+      int testInt = 0;
+      auto callback = callbackHelper->createCallback([&testInt](){ testInt++; });
+
+      THEN("The callback should execute as expected when the helper is in scope")
+      {
+        callback();
+        CHECK(testInt == 1);
+
+        AND_THEN("The callback shouldn't execute when the helper goes out of scope")
+        {
+          callbackHelper.reset(nullptr);
+          callback();
+          CHECK(testInt == 1);
+        }
+      }
+    }
+
+    WHEN("We create a callback using lambda that takes arguments with the callback helper")
+    {
+      auto callback = callbackHelper->createCallback([](int& intToIncrement, const int& intToLeaveAlone)
+                                                     {
+                                                       intToIncrement++;
+                                                     });
+
+      THEN("The callback should execute as expected")
+      {
+        int testInt = 0;
+        const int controlInt = 0;
+
+        callback(testInt, controlInt);
+        CHECK(testInt == 1);
+        CHECK(controlInt == 0);
+
+        AND_THEN("The callback shouldn't execute when the helper goes out of scope")
+        {
+          callbackHelper.reset(nullptr);
+          callback(testInt, controlInt);
+          CHECK(testInt == 1);
+          CHECK(controlInt == 0);
+        }
+      }
+    }
+  }
 }
 
